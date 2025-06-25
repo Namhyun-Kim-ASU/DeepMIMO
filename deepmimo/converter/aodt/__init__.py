dbf07136288e60483631c43aef92fa2f67e2b002
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-06-25 12:02:43 -0400

    add __init__ to allow for "from deepmimo.converter.aodt import aodt_exporter"

--- deepmimo/converter/aodt/__init__.py
+++ deepmimo/converter/aodt/__init__.py
@@ -1,9 +1,11 @@
-"""DeepMIMO AODT converter module.
+"""
+AODT (Aerial Omniverse Digital Twin) module for DeepMIMO.
 
-This module provides functionality for converting AODT ray tracing data
-into the DeepMIMO format.
+This module provides tools for exporting data to parquet format.
+Note: This functionality requires additional dependencies.
+Install them using: pip install 'deepmimo[aodt]'
 """
 
-__all__ = [
-    'aodt_rt_converter'
-] 
+from .aodt_exporter import aodt_exporter
+
+__all__ = ['aodt_exporter'] 
