cfb67af0e9ba9e6220d04602b642343304b3296e
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-06-26 13:32:14 -0400

    bump test version to 26 and adjust testing for dynamic (DONE)

--- testing.py
+++ testing.py
@@ -64,13 +64,15 @@ import os
 import deepmimo as dm
 import pandas as pd
 
-aodt_scen_name = 'aerial_2025_6_18_16_43_21'  # new
-# aodt_scen_name = 'aerial_2025_6_22_16_10_16' # old
+# aodt_scen_name = 'aerial_2025_6_18_16_43_21'  # new (1 user)
+# aodt_scen_name = 'aerial_2025_6_22_16_10_16' # old (2 users)
+aodt_scen_name = 'aerial_2025_6_18_16_43_21_dyn'  # new (1 user, dynamic)
 folder = f'aodt_scripts/{aodt_scen_name}'
 # df = pd.read_parquet(os.path.join(folder, 'db_info.parquet'))
 
 # df.head()
-dm.convert(folder, overwrite=True)
+aodt_scen = dm.convert(folder, overwrite=True)
+
+#%%
 
 aodt_scen = dm.load(aodt_scen_name, max_paths=500)
-
