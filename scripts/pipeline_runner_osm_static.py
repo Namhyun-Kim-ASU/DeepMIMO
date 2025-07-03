d9ea1ff7df758c95936470fad07f18938fa07fb4
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-07-02 20:45:16 -0400

    rename blender script

--- scripts/pipeline_runner_osm_static.py
+++ scripts/pipeline_runner_osm_static.py
@@ -13,7 +13,7 @@ import deepmimo as dm  # type: ignore
 
 from deepmimo.pipelines.TxRxPlacement import gen_rx_grid, gen_tx_pos, gen_plane_grid
 from deepmimo.pipelines.utils.pipeline_utils import get_origin_coords, load_params_from_row
-from deepmimo.pipelines.blender_osm_export import fetch_osm_scene
+from deepmimo.pipelines.blender_osm import fetch_osm_scene
 from deepmimo.pipelines.utils.geo_utils import get_city_name, fetch_satellite_view
 
 # API Keys
